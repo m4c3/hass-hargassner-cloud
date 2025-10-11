a934ba087641c4201f1d805b192acd852ffb54ad
Matthias Hähnel <github.com@drhaehnel.de>
2025-10-11 20:30:01 +0200

    Restore CONF_CLIENT_ID constant

--- custom_components/hargassner_cloud/const.py
+++ custom_components/hargassner_cloud/const.py
@@ -5,10 +5,11 @@ PLATFORMS = ["sensor"]
 
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
 CONF_INSTALLATION = "installation"
 
 DEFAULT_SCAN_INTERVAL = 60  # seconds
 
 AUTH_URL = "https://web.hargassner.at/api/auth/login"
-WIDGETS_URL_TMPL = "https://web.hargassner.at/api/widgets?installationId={installation}"
+WIDGETS_URL_TMPL = "https://web.hargassner.at/api/installations/{installation}/widgets"
